a345abb8c2c68095448adc343040335a85370d8c
ak220193 <mail2meak22frcrio@gmail.com>
2026-06-30 19:51:25 +0530

    Logo not Foound issues

--- founders_roi/app/components/sections/ClientMarque.jsx
+++ founders_roi/app/components/sections/ClientMarque.jsx
@@ -22,7 +22,7 @@ const clients = [
   { name: "Triberry Studios", logo: "/clientImages/triberrystudioslogo.png" },
   { name: "Kamarajar College of Nursing", logo: "/clientImages/KSAnursing.jpeg" },
   { name: "Kamarajar College of Pharmacy", logo: "/clientImages/KSA.jpeg" },
-  { name: "Webxode Technologies", logo: "/clientImages/webxodelogocropped-removebg-preview.png" },
+  { name: "Webxode Technologies", logo: "/clientImages/webxodelogocropped.png" },
   { name: "MagicLens Studio", logo: "/clientImages/MagicLens.jpg" },
   { name: "Infocus The Studio", logo: "/clientImages/Infocus.jpg" },
   { name: "Zero One Photography", logo: "/clientImages/ZeroOnePhotography.jpeg" },
